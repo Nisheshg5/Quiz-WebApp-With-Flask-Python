0162b050f2db08b6a69e4fceadb1667262b9e43f
Nishesh Garg <Nisheshg5@gmail.com>
2021-01-29 20:02:13 +0530

    Changed file structure

--- app.py
+++ app.py
@@ -1,3 +1,31 @@
-# Entry point for the application.
-from . import app  # For application discovery by the 'flask' command.
-from . import views  # For import side-effects of setting up routes.
+from datetime import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name=None):
+    return render_template("hello_there.html", name=name, date=datetime.now())
+
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
