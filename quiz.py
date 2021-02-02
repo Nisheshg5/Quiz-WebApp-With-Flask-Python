ed4f3ac8d5a457957a96f8136517e655cd2bf7a3
Nishesh Garg <Nisheshg5@gmail.com>
2021-02-03 01:49:28 +0530

    Passed whole quiz to the page

--- quiz.py
+++ quiz.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, redirect, session, url_for, flash
-from flask_login import current_user
 from datetime import datetime
+
+from flask import Blueprint, flash, redirect, render_template, session, url_for
+from flask_login import current_user
 
 from models import Quiz, User
 
@@ -11,6 +12,7 @@ quiz = Blueprint("quiz", __name__, static_folder="static", template_folder="temp
 def quiz_page():
     return redirect(url_for(**session["redirectURL"]))
 
+
 @quiz.route("/<quiz_id>")
 def quiz_page_id(quiz_id):
 
@@ -18,20 +20,16 @@ def quiz_page_id(quiz_id):
     if not current_user.is_authenticated:
         return redirect(url_for(**session["redirectURL"]))
 
-
     # should be a valid quiz_id
     quiz = Quiz.query.filter_by(quiz_id=quiz_id).first()
-    if(not quiz):
+    if not quiz:
         flash(message=["Invalid test code"], category="error")
         return redirect(url_for(**session["redirectURL"]))
 
     # check for quiz expiration
     # if the  quiz is expired we don't need to fetch the questions
     # expired
-    
-    if(quiz.end_date < datetime.utcnow()):        
-        return render_template("quiz.html", id=quiz_id, title=quiz.title, start=quiz.start_date, end=quiz.end_date)
-    else:
-        return render_template("quiz.html",  id=quiz_id, title=quiz.title, start=quiz.start_date, end=quiz.end_date, questions=quiz.questions)
 
-    
+    # if(quiz.end_date < datetime.utcnow()):
+    return render_template("quiz.html", quiz=quiz)
+
